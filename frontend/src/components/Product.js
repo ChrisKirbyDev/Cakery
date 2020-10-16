269247a942dbd6759067e6f27095936a089a89a4
Chris <chriskirbydev@gmail.com>
2020-10-16 15:48:57 +1300

    add product title to the card

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const Product = ({ product}) => {
+const Product = ({ product }) => {
   return (
     <Card className="my-3 py-3 rounded">
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+      </Card.Body>
     </Card>
   );
 };
