a02e85c32d7ecb967e2d4000448cf95eb08b6d26
Chris <chriskirbydev@gmail.com>
2020-10-28 13:45:21 +1300

    add redux in homescreen, useDispatch, useSelector

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import asyncHandler from "express-async-handler"
+import asyncHandler from "express-async-handler";
 const router = express.Router();
 import Product from "../models/productModel.js";
 
@@ -7,25 +7,30 @@ import Product from "../models/productModel.js";
 // @route GET /api/products
 // @access Public
 
-router.get("/", asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-  res.json(products);
-}));
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc Fetch single products
 // @route GET /api/products/:id
 // @access Public
 
-router.get("/:id", asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-  if(product) {
-    res.json(product);
-  } else {
-      res.status(404)
-      throw new Error('Product not found')
-  }
-  
-}));
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  })
+);
 
 export default router;
